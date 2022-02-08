7fe37db0d21116396d31d7aecc8996eb9fb930fe
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-02-08 10:13:34 +0100

    n1e1 package

--- src/itacademy/lsr/javalanguage/task01/n1e01/Vehicle.java
+++ src/itacademy/lsr/javalanguage/task01/n1e01/Vehicle.java
@@ -2,7 +2,28 @@ package itacademy.lsr.javalanguage.task01.n1e01;
 
 public class Vehicle {
 
-    public static void main(String[] args) {
-        // write your code here
+    public String name;
+    public String state = "off";
+
+
+    public Vehicle (String newName){
+        name = newName;
     }
+
+    public void iniciar() {
+        if (state.equals("off")){
+            System.out.println("Turing on...");
+            state = "on";
+        }
+    }
+
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle("Car");
+        System.out.println(car.name + " is " + car.state);
+        car.iniciar();
+        System.out.println(car.name + " is " + car.state);
+
+    }
+
+
 }
