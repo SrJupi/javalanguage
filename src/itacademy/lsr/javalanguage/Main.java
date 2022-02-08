fdeb4b9529e10d0d1636968cbdc281747df4c8eb
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-02-08 09:52:28 +0100

    Initial Commit

---
+++ src/itacademy/lsr/javalanguage/Main.java
@@ -0,0 +1,8 @@
+package itacademy.lsr.javalanguage;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/itacademy/lsr/javalanguage/task01/n1e01/Vehicle.java
@@ -0,0 +1,8 @@
+package itacademy.lsr.javalanguage.task01.n1e01;
+
+public class Vehicle {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
