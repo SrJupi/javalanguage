1d0428063db6339f58014a37a1540acaf30e1905
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-02-14 10:20:10 +0100

    task2 - n3e2 package

--- src/itacademy/lsr/javalanguage/task02/n3e02/Main.java
+++ src/itacademy/lsr/javalanguage/task02/n3e02/Main.java
@@ -2,25 +2,20 @@ package itacademy.lsr.javalanguage.task02.n3e02;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            try{
-                System.out.println("Creating new FailingConstructor with ID: " + i);
-                FailingConstructor fc = new FailingConstructor(i);
-
-            } catch (Exception e) {
-                System.out.println(i + ": " + e.getMessage());
-            }
+        try{
+            FailingConstructor fc = new FailingConstructor(123);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("End");
         }
     }
 }
 
+
 class FailingConstructor {
     int id;
     FailingConstructor(int id) throws Exception {
-        if(id>5){
-            throw new Exception ("Outside ID scope");
-        }
         this.id = id;
-
     }
 }
