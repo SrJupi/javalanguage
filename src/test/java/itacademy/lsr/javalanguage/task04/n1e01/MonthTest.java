b3ca28bac863b57dc9181a93db91a9a8a3c92ee7
SrJupi <79888279+SrJupi@users.noreply.github.com>
2022-02-23 10:28:40 +0100

    task4 - n1e1

---
+++ src/test/java/itacademy/lsr/javalanguage/task04/n1e01/MonthTest.java
@@ -0,0 +1,32 @@
+package itacademy.lsr.javalanguage.task04.n1e01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MonthTest{
+    @Test
+    void listHave12Months (){
+        Month month = new Month();
+        assertEquals(12, month.getListSize());
+    }
+
+    @Test
+    void listIsNull (){
+        Month month = new Month();
+        assertNotNull(month.getList());
+    }
+
+    @Test
+    void augustIs8thMonth (){
+        Month month = new Month();
+        assertEquals(7, month.getIndex("August"));
+    }
+
+    @Test
+    void _8thMonthIsAugust () {
+        Month month = new Month();
+        assertEquals("August", month.getMonth(7));
+    }
+}
